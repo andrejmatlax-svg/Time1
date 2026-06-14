d9684f13929f6e18e63cc27049f56890362da698
Andrey <Matlah>
2026-06-14 11:37:58 +0300

    enum - Timer

--- testode/testode/ViewController.swift
+++ testode/testode/ViewController.swift
@@ -4,6 +4,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-   
+//MARK: Свойства
+    enum Timer{
+        case work
+        case rest
+        
+        
+        var duration: Int {
+        switch self {
+        case .work: return 25
+        case .rest: return 10
+                    
+            }
+        }
+        
+        
+        
+        
+    }
+                    
+                    
 }
