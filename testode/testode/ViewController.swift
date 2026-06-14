6ab91958f4ca7ed0c8d4afe71cf0aab80f4d0325
Andrey <Matlah>
2026-06-14 11:41:46 +0300

    Color (work), (rest)

--- testode/testode/ViewController.swift
+++ testode/testode/ViewController.swift
@@ -11,17 +11,23 @@ class ViewController: UIViewController {
         
         
         var duration: Int {
-        switch self {
-        case .work: return 25
-        case .rest: return 10
-                    
+            switch self {
+            case .work: return 25
+            case .rest: return 10
+                
             }
         }
         
-        
-        
-        
+        var Color: UIColor {
+            switch self {
+            case .work: return UIColor(red: 0.92, green: 0.48, blue: 0.44, alpha: 1.0)
+            case .rest: return UIColor(red: 0.40, green: 0.76, blue: 0.60, alpha: 1.0)
+            }
+        }
     }
-                    
+      
+    
+    
+    
                     
 }
